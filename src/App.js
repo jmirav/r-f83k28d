c1c9c2459ffc5fde256d3345fa4d67f0eeb4fdc0
jmirav <jmira420@gmail.com>
2018-04-05 14:15:00 -0500

    Fixes increment function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
+let nmb = 0
+
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      number: 0
+    };
+  }
+
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value" >{this.state.number}</span>
+        <button id="inc" onClick={this.Increment.bind(this)}>Incrementa</button>
       </div>
     );
+  }
+
+  Increment = () => {
+    this.setState({ number: this.state.number + 1 });
   }
 }
 
